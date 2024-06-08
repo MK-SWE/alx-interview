ad2133446037447a9dfaddefa0046b0f5547d512
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2024-06-08 17:19:37 +0300

    starwar api

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,19 +3,25 @@ const request = require('request');
 const url = 'https://swapi-api.alx-tools.com/api/';
 const movieID = process.argv[2];
 
+function getChar(url, i){
+  if (url.length === i) {
+    return;
+  }
+  request({ uri: url[i], json: true }, (error, respone) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(respone.body.name)
+      getChar(url, i+1)
+    }
+  });
+}
+
 request({ uri: `${url}films/${movieID}`, json: true }, (error, respone) => {
   if (error) {
     console.log(error);
   } else {
     const charLst = respone.body.characters;
-    charLst.map(el => {
-      request({ url: el, json: true }, (error, respone) => {
-        if (error) {
-          console.log(error);
-        } else {
-          console.log(respone.body.name);
-        }
-      });
-    });
+    getChar(charLst, 0)
   }
 });
