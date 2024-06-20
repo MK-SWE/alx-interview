21f3ff7354afa4ee58cbfbfe30590d5c88fd9041
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2024-06-21 00:02:41 +0300

    make change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -8,12 +8,17 @@ from typing import List
 def makeChange(coins: List, total: int) -> int:
     """ Main Entrypoint
     """
-    minCoins = [float('inf')] * (total + 1)
-    minCoins[0] = 0
+    if not coins or coins is None:
+        return -1
+    if total <= 0:
+        return 0
+    steps = 0
+    coins = reversed(sorted(coins))
 
     for coin in coins:
-        for i in range(coin, total + 1):
-            if coin <= i:
-                minCoins[i] = min(minCoins[i], minCoins[i - coin] + 1)
-
-    return -1 if minCoins[total] == float('inf') else minCoins[total]
+        while coin <= total:
+            total -= coin
+            steps += 1
+        if (total == 0):
+            return steps
+    return -1
