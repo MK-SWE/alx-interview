ec8110e8705f3f1245ef8a540b049d5f7cca3bb5
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2024-06-27 20:06:35 +0300

    Island Perimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Island Perimeter
+"""
+
+def island_perimeter(grid):
+    """
+    Create a function island_perimeter(grid):
+    that returns the perimeter of the island described in grid:
+    """
