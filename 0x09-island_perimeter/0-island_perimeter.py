8a997dace43d0a0901bf199f1dac09bb3a7759a8
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2024-06-27 20:08:54 +0300

    Island Perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -2,8 +2,14 @@
 """Island Perimeter
 """
 
+
 def island_perimeter(grid):
     """
     Create a function island_perimeter(grid):
     that returns the perimeter of the island described in grid:
     """
+    if grid is None:
+        return 0
+    else:
+        # Add your code here to calculate the perimeter
+        pass
