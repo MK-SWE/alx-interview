2835a649167e3dc6d0d7e51f1da45adb7d3e501d
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2024-06-28 01:21:10 +0300

    island_perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,15 +1,37 @@
 #!/usr/bin/python3
 """Island Perimeter
 """
+from typing import List
 
 
-def island_perimeter(grid):
+def island_perimeter(grid: List[List[int]]) -> int:
     """
     Create a function island_perimeter(grid):
     that returns the perimeter of the island described in grid:
     """
-    if grid is None:
+    def dfs(rowIndex: int, colIndex: int) -> int:
+        """
+        for out of bound items, return 1
+        for water, return 1, because this side is a perimeter
+        for '-1' return 0, because we have been there
+        Change the grid value to '-1' to mark it as visited
+        """
+        if rowIndex < 0 or rowIndex > len(grid)-1 or \
+           colIndex < 0 or colIndex > len(grid[rowIndex])-1:
+            return 1
+        elif grid[rowIndex][colIndex] == -1:
+            return 0
+        elif grid[rowIndex][colIndex] == 0:
+            return 1
+        else:
+            grid[rowIndex][colIndex] = -1
+            return dfs(rowIndex, colIndex - 1) + dfs(rowIndex - 1, colIndex) +\
+                dfs(rowIndex, colIndex + 1) + dfs(rowIndex + 1, colIndex)
+
+    if grid is None or len(grid) == 0:
         return 0
     else:
-        # Add your code here to calculate the perimeter
-        pass
+        for current_row in range(len(grid)):
+            for current_col in range(len(grid[current_row])):
+                if grid[current_row][current_col] == 1:
+                    return dfs(current_row, current_col)
